c7b36842a2af4b4d6bc35272db608daaec76ca03
eric <ericnascimentocruzc@gmail.com>
2023-05-16 15:14:38 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import PageNotFound from './pages/PageNotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+          <Route exact path="/profile/Edit" component={ ProfileEdit } />
+          <Route exact path="/" component={ Login } />
+          <Route path="" component={ PageNotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PageNotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="page-not-found">page-not-found</p>
+      </div>
+    );
+  }
+}
+
+export default PageNotFound;
