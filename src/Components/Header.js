e204d17f47c3e0d9c967330416983c924003030a
eric <ericnascimentocruzc@gmail.com>
2023-05-17 07:21:06 -0300

    Requisito 4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -19,7 +20,9 @@ class Header extends React.Component {
     const { login, users } = this.state;
     return (
       <header data-testid="header-component">
-        {console.log(users)}
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
         {login ? <p>Carregando...</p> : <p data-testid="header-user-name">{users}</p>}
       </header>
     );
